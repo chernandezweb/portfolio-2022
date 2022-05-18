a12fe36dec883a32709065417c2d985f39507938
Carlos <chernandezweb@gmail.com>
2022-05-17 21:12:33 -0400

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,6 +8,7 @@ const Navigation = props => {
             <nav>
                 <Link to="/">Home</Link>
                 {/* <Link to="/about">About</Link> */}
+
             </nav>
         </div>
     )
